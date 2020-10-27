184847f6ccfaeddffc3f4ab21e811a336f48f4ca
Taylor <70418358+taysingleton@users.noreply.github.com>
2020-10-26 22:53:03 -0400

    Add files via upload

---
+++ SierraOffCampusHousing/src/ReviewJSON.java
@@ -0,0 +1,20 @@
+/**
+ * Taylor 
+ */
+{"Review":[
+{
+	"reviews": "It's a nice apartment",
+	"rating": 4,
+	"user": "Jack H."
+},{
+	"reviews": "It's not as good as it looks in the pictures",
+	"rating": 2,
+	"user": "Corey L."
+},{
+	"reviews": "Super great management",
+	"rating": 5,
+	"user": "Kayla A."
+}
+
+
+]}
