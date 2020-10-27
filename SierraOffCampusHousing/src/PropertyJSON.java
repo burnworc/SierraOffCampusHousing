cbe49121b1da980c7dddb32628c1c5b6bee44ac4
Taylor <70418358+taysingleton@users.noreply.github.com>
2020-10-26 23:34:11 -0400

    Add files via upload

--- SierraOffCampusHousing/src/PropertyJSON.java
+++ SierraOffCampusHousing/src/PropertyJSON.java
@@ -6,13 +6,15 @@
 
 {"Property":[
   {
-     "Name" : Stadium Suites,
+     "id": 1,
+	 "Name" : Stadium Suites,
      "Price" : 640,
      "Location": Columbia,
      "Size", 
      "NumOfRooms": 4,
      "Rating": 3
 },{
+	"id": 2,
 	"Name" : 21 Oaks,
 	"Price" : 560,
 	"Location": Columbia,
@@ -21,6 +23,7 @@
 	'Rating': 3
 	
 },{
+	"id": 3,
 	"Name": Redtail,
     "Price" : 670,
     "Location": Columbia,
--- SierraOffCampusHousing/src/ReviewJSON.java
+++ SierraOffCampusHousing/src/ReviewJSON.java
@@ -3,14 +3,17 @@
  */
 {"Review":[
 {
+	"id":1,
 	"reviews": "It's a nice apartment",
 	"rating": 4,
 	"user": "Jack H."
 },{
+	"id":2,
 	"reviews": "It's not as good as it looks in the pictures",
 	"rating": 2,
 	"user": "Corey L."
 },{
+	"id":3,
 	"reviews": "Super great management",
 	"rating": 5,
 	"user": "Kayla A."
